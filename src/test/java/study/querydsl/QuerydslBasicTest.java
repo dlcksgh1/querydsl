4e47246f9596581471757fcd7513ee2f522ad260
Lee  Chanho <sma05091@nate.com>
2023-09-15 21:34:27 +0900

    검색 조건 쿼리

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,6 @@
 package study.querydsl;
 
+import com.querydsl.core.QueryResults;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +13,8 @@ import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static study.querydsl.entity.QMember.member;
@@ -106,4 +109,74 @@ public class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    public void search() throws Exception {
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1")
+                        .and(member.age.eq(10)))
+                .fetchOne();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+
+
+        // JPQL이 제공하는 모든 검색조건 제공
+        member.username.eq("member1"); // username = 'member1'
+        member.username.ne("member1"); // username != 'member1'
+        member.username.eq("member1").not(); // username != 'member1'
+
+        member.username.isNotNull(); // 이름이 is not null
+
+        member.age.in(10, 20); // age in(10, 20)
+        member.age.notIn(10, 20); // age not in(10, 20)
+        member.age.between(10, 30); //between 10, 30
+
+        member.age.goe(30); // age >= 30
+        member.age.gt(30); // age > 30
+        member.age.loe(30); // age <= 30
+        member.age.lt(30); // age < 30
+
+        member.username.like("member%"); //like 검색
+        member.username.contains("member"); // like ‘%member%’ 검색
+        member.username.startsWith("member"); //like ‘member%’ 검색
+    }
+
+    @Test
+    public void searchAndParam() throws Exception {
+        List<Member> result1 = queryFactory
+                .selectFrom(member)
+                .where(member.username.eq("member1"),
+                        member.age.eq(10))
+                .fetch();
+
+        assertThat(result1.size()).isEqualTo(1);
+
+        //List
+        List<Member> fetch = queryFactory
+                .selectFrom(member)
+                .fetch();
+
+        //단 건
+        Member findMember1 = queryFactory
+                .selectFrom(member)
+                .fetchOne();
+
+        //처음 한 건 조회
+        Member findMember2 = queryFactory
+                .selectFrom(member)
+                .fetchFirst();
+
+        //페이징에서 사용
+        QueryResults<Member> results = queryFactory
+                .selectFrom(member)
+                .fetchResults();
+
+        //count 쿼리로 변경
+        long count = queryFactory
+                .selectFrom(member)
+                .fetchCount();
+    }
+
 }
